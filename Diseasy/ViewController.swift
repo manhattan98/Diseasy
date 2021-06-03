dbd26d749c611a5d04a8a53e1fe07f84d84f0193
Eremej <choom1998@gmail.com>
2021-06-03 11:29:51 +0700

    Initial Commit

---
+++ Diseasy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diseasy
+//
+//  Created by Eremej Sumcenko on 03.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
